c5d6605528e604b6a9ad14c79f96daaf8323e523
Anukul <anukulpk2@gmail.com>
2025-02-09 22:46:38 -0500

    Database config added

---
+++ backend/src/main/java/com/budgetingapp/backend/BudgetingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.budgetingapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetingBackendApplication.class, args);
+	}
+
+}
