3c8ee5563e8f71b922ee150acd6d5c158770b8ab
Anukul <anukulpk2@gmail.com>
2025-02-12 22:53:16 -0500

    Added post API

--- backend/src/main/java/com/budgetingapp/backend/controller/ExpenseController.java
+++ backend/src/main/java/com/budgetingapp/backend/controller/ExpenseController.java
@@ -1,11 +1,12 @@
 package com.budgetingapp.backend.controller;
 
+import com.budgetingapp.backend.dto.ExpenseDTO;
 import com.budgetingapp.backend.model.Expense;
 import com.budgetingapp.backend.repository.ExpenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +17,29 @@ public class ExpenseController {
     @Autowired
     private ExpenseRepository expenseRepository;
 
+    public ExpenseController(ExpenseRepository expenseRepository){
+        this.expenseRepository=expenseRepository;
+    }
+
     @GetMapping
     public List<Expense> getAllExpenses(){
-        return expenseRepository.findAll();
+        List<Expense> expenses = expenseRepository.findAll();
+        return expenses;
+    }
+
+    @PostMapping
+    public ResponseEntity<Expense> addExpense(@RequestBody ExpenseDTO expenseDTO){
+        Expense expense = new Expense(
+                expenseDTO.getDescription(),
+                expenseDTO.getAmount(),
+                expenseDTO.getDate(),
+                expenseDTO.getCategory(),
+                expenseDTO.getCurrency(),
+                expenseDTO.getNotes()
+        );
+        Expense savedExpense = expenseRepository.save(expense);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedExpense);
     }
 
 }
